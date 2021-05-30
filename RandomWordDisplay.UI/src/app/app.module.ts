e5eab8b2fe55e27983753fc55f15485427ad4b88
allen-chin <chin.allen.cp@gmail.com>
2021-05-30 08:01:58 -0700

    Updated app.module.ts with necessary ServerStateComponent imports

--- RandomWordDisplay.UI/src/app/app.module.ts
+++ RandomWordDisplay.UI/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ServerStateComponent } from './server-state/server-state.component';
@@ -10,7 +11,8 @@ import { ServerStateComponent } from './server-state/server-state.component';
     ServerStateComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
