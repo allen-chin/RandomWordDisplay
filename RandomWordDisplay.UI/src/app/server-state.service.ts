b363f82d713ff36c5c908cb27dd55c2bb7d04076
allen-chin <chin.allen.cp@gmail.com>
2021-05-30 08:03:31 -0700

    Update ServerStartService with Start POST endpoint

--- RandomWordDisplay.UI/src/app/server-state.service.ts
+++ RandomWordDisplay.UI/src/app/server-state.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { ServerState } from './server-state.model'
 
@@ -8,13 +9,35 @@ import { ServerState } from './server-state.model'
   providedIn: 'root'
 })
 export class ServerStateService {
-  private baseUrl = "https://localhost:5000/api/State";
+  private baseUrl = "https://localhost:5000/api";
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private http: HttpClient) { }
 
+  // TODO: Replace this with web sockets
   getServerState(): Observable<ServerState> {
-    const url = `${this.baseUrl}/`;
-    console.log(this.http.get<ServerState>(url));
+    const url = `${this.baseUrl}/State`;
     return this.http.get<ServerState>(url);
+  }
+
+  postStartCommand(words: string[]): void {
+    const url = `${this.baseUrl}/Start`;
+    const body = JSON.stringify(words);
+    const result = this.http.post<void>(url, body, this.httpOptions)
+      .pipe(
+        tap(_ => console.log(`Successfully posted start command.`)),
+        catchError(this.handleError<string>('postStartCommand'))
+      ).subscribe();
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
