d7a50d28e9fb6405539626a8eb9aaeb10c57a24b
allen-chin <chin.allen.cp@gmail.com>
2021-05-30 02:14:37 -0700

    Add Start endpoint

---
+++ RandomWordDisplay.API/Controllers/StartController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RandomWordDisplay.API.Services;
+using System;
+using System.Threading;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RandomWordDisplay.API.Controllers
+{
+    [Route("api/Start")]
+    [ApiController]
+    public class StartController : ControllerBase
+    {
+        private readonly ILogger<StartController> _logger;
+        private readonly IRandomWordService _randomWordService;
+
+        public StartController(ILogger<StartController> logger, IRandomWordService randomWordService)
+        {
+            _logger = logger;
+            _randomWordService = randomWordService;
+        }
+
+        // POST api/<StartController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string[] wordList)
+        {
+            bool success = _randomWordService.Configure(wordList);
+
+            if (success)
+            {
+                _randomWordService.StartAsync(new CancellationToken());
+                return Ok();
+            }
+
+            return BadRequest("Command is still running.");
+        }
+    }
+}
