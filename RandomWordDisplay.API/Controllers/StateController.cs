b35016088116f226334274355eeaf7eb027d07b2
allen-chin <chin.allen.cp@gmail.com>
2021-05-30 05:48:56 -0700

    Create StateController.cs

---
+++ RandomWordDisplay.API/Controllers/StateController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RandomWordDisplay.API.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RandomWordDisplay.API.Controllers
+{
+    [Route("api/State")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        private readonly ILogger<StartController> _logger;
+        private readonly IRandomWordService _randomWordService;
+
+        public StateController(ILogger<StartController> logger, IRandomWordService randomWordService)
+        {
+            _logger = logger;
+            _randomWordService = randomWordService;
+        }
+
+        // GET: api/<StateController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                _randomWordService.CommandRunning,
+                _randomWordService.CommandTimeRemaining,
+                _randomWordService.CurrentWordSelected
+            });
+        }
+    }
+}
